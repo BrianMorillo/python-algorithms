81437953efa42d42f9f8ced3a13c76d81d204fac
BrianMorillo <brian.morillo.94@gmail.com>
2023-11-26 00:00:05 -0500

    updated tests to include not only quick sort but also insertion sort

--- test_sorting_algorithms.py
+++ test_sorting_algorithms.py
@@ -4,35 +4,41 @@
 
 Randomized Quick Sort test
 """
-from quicksort import quick_sort 
 import pytest
+from insertion_sort import insertion_sort
+from quicksort import quick_sort
+algorithm_list = [insertion_sort, quick_sort]
 
 # Test case 1: Sort an empty list
-def test_empty_list():
+@pytest.mark.parametrize("sorting_algorithm", algorithm_list)
+def test_empty_list(sorting_algorithm):
     input_list = []
     expected_output = []
-    quick_sort(input_list)
+    sorting_algorithm(input_list)
     assert input_list == expected_output
 
 # Test case 2: Sort a list with one element
-def test_single_element_list():
+@pytest.mark.parametrize("sorting_algorithm", algorithm_list)
+def test_single_element_list(sorting_algorithm):
     input_list = [42]
     expected_output = [42]
-    quick_sort(input_list)
+    sorting_algorithm(input_list)
     assert input_list == expected_output
 
 # Test case 3: Sort a list with multiple elements
-def test_multi_element_list():
+@pytest.mark.parametrize("sorting_algorithm", algorithm_list)
+def test_multi_element_list(sorting_algorithm):
     input_list = [9, 7, 5, 11, 12, 2, 14]
     expected_output = [2, 5, 7, 9, 11, 12, 14]
-    quick_sort(input_list)
+    sorting_algorithm(input_list)
     assert input_list == expected_output
     
 # Test case 4: Sort a list with some duplicate elements
-def test_duplicate_element_list():
+@pytest.mark.parametrize("sorting_algorithm", algorithm_list)
+def test_duplicate_element_list(sorting_algorithm):
     input_list = [9, 9, 9, 11, 2, 2, 14]
     expected_output = [2, 2, 9, 9, 9, 11, 14]
-    quick_sort(input_list)
+    sorting_algorithm(input_list)
     assert input_list == expected_output    
 
 if __name__ == '__main__':
